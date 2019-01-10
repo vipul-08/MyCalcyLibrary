344b7531779bf036677f1082848878c834b02704
vipul-08 <singh.vipul15@siesgst.ac.in>
2019-01-10 14:47:28 +0530

    New Library

---
+++ mycalcy/src/main/java/com/mobicule/mycalcy/Calculator.java
@@ -0,0 +1,29 @@
+package com.mobicule.mycalcy;
+
+public class Calculator {
+
+    public int add(int a , int b) {
+        return a+b;
+    }
+
+    public int subtract(int a , int b) {
+        return a+b;
+    }
+
+    public int multiply(int a , int b) {
+        return a+b;
+    }
+
+    public int divide(int a , int b) {
+        return a+b;
+    }
+
+    public int modulus(int a , int b) {
+        return a+b;
+    }
+
+    public double power(int a , int b) {
+        return Math.pow(a,b);
+    }
+
+}
